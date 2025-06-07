ec39483482c8ecc08cb27c101ce8a42a9703454d
Beverly Ndukwe <ndukwebeverly.com>
2025-06-07 22:46:12 +0300

    Initialise commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+const hq=42;
+return pickup>hq? pickup-hq:hq-pickup;
+}
+function distanceFromHqInFeet(pickup){
+    return distanceFromHqInBlocks(pickup)*264;
+}
+function distanceTravelledInFeet(start, destination){
+    return (start>destination? start-destination:destination-start)*264;
+}
+function calculatesFarePrice(start,destination){
+    const distance=distanceTravelledInFeet(start,destination);
+
+    if(distance<=400){
+        return 0;
+    }else if(distance<=2000){
+        return(distance-400)*0.02;
+    }else if(distance<=2500){
+        return 25;
+    }else{
+        return'cannot travel that far';
+    }
+}
